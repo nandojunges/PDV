87837252e811631c2fa248f40a825f1ff8583ac9
nandojunges <ferjunges@outlook.com>
2026-01-16 14:05:50 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/pdvsaoloureco/app/MainActivity.java
+++ android/app/src/main/java/com/pdvsaoloureco/app/MainActivity.java
@@ -1,25 +1,15 @@
 package com.pdvsaoloureco.app;
 
 import android.os.Bundle;
-import android.util.Log;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try {
-            // Registro explícito do plugin (necessário quando não existe auto-registro no projeto)
-            registerPlugin(AndroidPrinterPlugin.class);
-            Log.i(TAG, "AndroidPrinterPlugin registrado com sucesso");
-        } catch (Throwable t) {
-            Log.e(TAG, "Falha ao registrar AndroidPrinterPlugin", t);
-        }
-
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "MainActivity onCreate finalizado");
+
+        // Registra o plugin de impressão
+        registerPlugin(AndroidPrinterPlugin.class);
     }
 }
